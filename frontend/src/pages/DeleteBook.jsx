b1e887a4e9a14e677aee37453bc85fba1596c0a7
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-08-26 21:30:13 +0300

    feat: implement DeleteBook page with loading state and confirmation prompt; add BackButton and Spinner components

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useNavigate, useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useState } from "react";
 
 function DeleteBook() {
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then((res) => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
   return (
-    <div>DeleteBook</div>
-  )
+    <div className="p-4">
+      <BackButton />
+      <h1>Delete Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-2xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl">Are You Sure Do You Want To Delete This Book?</h3>
+        <button className="px-4 py-2 rounded-2xl bg-red-600 text-white mt-4  cursor-pointer" onClick={handleDeleteBook}>Delete</button>
+      </div>
+    </div>
+  );
 }
 
-export default DeleteBook
+export default DeleteBook;
