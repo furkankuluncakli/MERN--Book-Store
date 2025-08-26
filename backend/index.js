d53669de87af01bec523a2457fc3ce00417ffec8
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-08-26 19:11:51 +0300

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json for frontend dependencies and scripts.
    - Created Vite configuration for React and Tailwind CSS.
    - Implemented main application structure with routing.
    - Developed pages for creating, editing, deleting, and showing books.
    - Added a loading spinner component for better UX.
    - Integrated Axios for API calls to fetch book data.
    - Styled components using Tailwind CSS for a responsive design.

--- backend/index.js
+++ backend/index.js
@@ -1,98 +1,26 @@
 import express, { json } from "express";
 import { mongoDBURL, PORT } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 app.get("/", (req, res) => {
   return res.status(200).send("AMUNAGOYUM");
 });
 
-app.post("/books", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res.status(400).send({ message: "Please Provide All Fields" });
-    }
-
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      publishYear: req.body.publishYear,
-    };
-
-    const book = await Book.create(newBook);
-
-    return res.status(201).send(book);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send({ message: error.message });
-  }
-});
-
-app.get("/books", async (req, res) => {
-  try {
-    const books = await Book.find({});
-    res.status(200).json({
-      count: books.length,
-      data: books,
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send({ message: error.message });
-  }
-});
-
-app.get("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const book = await Book.findById(id);
-
-    return res.status(200).json(book);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send({ message: error.message });
-  }
-});
-
-app.put("/books/:id", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res.status(400).send({ message: "Please Provide All Fields" });
-    }
-
-    const { id } = req.params;
-
-    const result = await Book.findByIdAndUpdate(id, req.body);
-
-    if (!result) {
-      return res.status(404).json({ message: "Book Not Found" });
-    } else {
-      res.status(200).send({ message: "Book updated succesfully" });
-    }
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
-});
-
-app.delete("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const result = await Book.findByIdAndDelete(id);
-
-    if (!result) {
-      res.status(404).json({ message: "Book not found" });
-    } else {
-      res.status(200).send({ message: "Book deleted succesfully" });
-    }
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
-});
+app.use("/books", booksRoute);
 
 mongoose
   .connect(mongoDBURL)
